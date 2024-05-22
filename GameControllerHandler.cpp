d5843d9b4cb6bf2ea9fcd0aadb8741743a78be76
ackimixs <ackimixs@protonmail.com>
2024-05-22 15:36:29 +0200

    detection range

--- GameControllerHandler.cpp
+++ GameControllerHandler.cpp
@@ -46,7 +46,7 @@ void GameControllerHandler::handleMessage(const std::string &message) {
             if (distance <= 100) {
                 strength = 0xFFFF;
             } else if (distance <= this->lidarDectectionDistance / 2) {
-                double factor = 1 - Modelec::mapValue(distance, 100.0, this->lidarDectectionDistance, 0.0, 1.0);
+                double factor = 1 - Modelec::mapValue(distance, 100.0, this->lidarDectectionDistance / 2, 0.0, 1.0);
                 strength = static_cast<Uint16>(factor * 0xFFFF);
             } else {
                 strength = 0;
